4726ba06fc892bbf9f17985098f12be19f2c24d5
Alexander Fengler <afengler@node474.oscar.ccv.brown.edu>
2019-07-15 16:06:59 -0400

    making changes to model fitting script

--- torch_fit_model.py
+++ torch_fit_model.py
@@ -2,75 +2,113 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import numpy as np
-from itertools import product
 from cddm_data_simulation import ddm_simulate
-from cdweiner import fptd
+from cdwiener import array_fptd
 import os
+import pandas as pd
+import scipy as scp
+import scipy.stats as scps
+import time
+from datetime import datetime
+import yaml
 
 from torch_models import Net
+from kde_training_utilities import kde_load_data
+from kde_training_utilities import kde_make_train_test_split
 
-def generate_input_grid():
-    grid_v = np.random.uniform(-1, 1, 10)
-    grid_a = np.random.uniform(0, 1.5, 10)
-    grid_z = np.random.uniform(0, 1, 10)
-    grid_rt = np.random.uniform(0, 5, 10)
-    grid_choice = [-1, 1]
-    return torch.tensor(list(product(grid_v, grid_a, grid_z, grid_rt, grid_choice)))
-    
-def generate_labels(input_grid):
-    out = [fptd(t=feature[4] * feature[3], v=feature[0], a=feature[1], w=feature[2] * feature[1], 
-        eps = 1e-8) for feature in input_grid]
-    return torch.tensor(out)
+data_folder = "/users/afengler/data/kde/ddm/train_test_data/"
 
-def train_test(X, y, X_val, y_val, learning_rate=.002, n_batches=5, n_epochs=20, save=True):
-    PATH = "/home/tony/repos/temp_models/torch_dnnregressor_ddm"
-    if not os.path.exists(PATH):
-        os.makedirs(PATH)
+device = torch.device("cuda")
 
-    loss_fn = nn.MSELoss()
-    network = Net()
-    optimizer = optim.Adam(network.parameters(), lr=learning_rate)
+# kde_make_train_test_split(folder = data_folder,
+#                           p_train = 0.8)
 
-    batch_size = X.shape[0] // n_batches
+# Load train test split
+X, _, X_val, __ = kde_load_data(folder = data_folder, log=True, prelog_cutoff='none')
 
-    for epoch in range(n_epochs):
-        for i in range(n_batches):
-            X_batch = X[i * batch_size:(i+1) * batch_size]
-            y_batch = y[i * batch_size:(i+1) * batch_size]
+# Use the analytical likelihoods for training instead
+X = np.array(X)
+y = np.log(array_fptd(X[:, 3] * X[:, 4] * (-1), X[:, 0], X[:, 1], X[:, 2]))
+X_val = np.array(X_val)
+y_val = np.log(array_fptd(X_val[:, 3] * X_val[:, 4] * (-1), X_val[:, 0], X_val[:, 1], X_val[:, 2]))
 
-            optimizer.zero_grad()
-            out = network(X_batch)
-            loss = loss_fn(out, y_batch)
-            loss.backward()
-            optimizer.step()
+# Convert to pytorch tensors
+X = torch.tensor(X, dtype=torch.float32)
+y = torch.tensor(y, dtype=torch.float32)
+# X_val = torch.tensor(X_val, dtype=torch.float32)
+# y_val = torch.tensor(y_val, dtype=torch.float32)
 
-            out = network(X_val)
-            loss = loss_fn(out, y_val)
-            print("epoch {}, batch {}, loss: {}".format(epoch, i, loss))
+X = X.to(device)
+y = y.to(device)
+# X_val = X_val.to(device)
+# y_val = y_val.to(device)
+
+with open("/users/afengler/git_repos/tony/nn_likelihoods/hyperparameters.yaml") as f:
+    params = yaml.load(f)
+
+input_shape = X.shape[1]
+timestamp = datetime.now().strftime('%m_%d_%y_%H_%M_%S')
+
+# Data params
+
+model_path  = "/users/afengler/data/tony/kde/ddm/torch_models"
+model_path += "/" + params["model_type"] + params["data_type_signature"] + timestamp
+
+if not os.path.exists(model_path):
+    os.makedirs(model_path)
+
+# Log hyperparameters
+os.system("cp {} {}".format("/users/afengler/git_repos/tony/nn_likelihoods/hyperparameters.yaml", model_path + "/"))
+
+loss_fn = nn.MSELoss()
+network = Net(input_dim=input_shape, num_layers=len(params["hidden_layers"]), 
+	layer_sizes= params["hidden_layers"], layer_activations=params["hidden_activations"])
+network = network.cuda()
+print("is model on CUDA? {}".format(next(network.parameters()).is_cuda))
+optimizer = optim.Adam(network.parameters(), lr=params["learning_rate"])
+
+print(network)
+
+n_batches = X.shape[0] // params["batch_size"]
+
+# Instead of moving the entire dataset onto the GPU, move each batch to the GPU
+# at each epoch
+
+for epoch in range(params["n_epochs"]):
+    for i in range(n_batches):
+        X_batch = X[i * params["batch_size"]:(i+1) * params["batch_size"]]
+        y_batch = y[i * params["batch_size"]:(i+1) * params["batch_size"]]
+
+        # X_batch = X_batch.to(device)
+        # y_batch = y_batch.to(device)
+
+        optimizer.zero_grad()
+        out = network.forward(X_batch)
+        loss = loss_fn(out, y_batch)
+        print("epoch {}, batch {}, loss: {}".format(epoch, i, loss))
+        loss.backward()
+        optimizer.step()
+
+        # out_val = network(X_val)
+        # loss_val = loss_fn(out, y_val)
+        # print("epoch {}, batch {}, loss: {}".format(epoch, i, loss_val))
 
         if epoch % 10 == 0:
             torch.save({
-                "epoch": epoch,
-                "model_state_dict": network.state_dict,
-                "optim_state_dict": optimizer.state_dict,
-                "loss": loss
-                }, PATH + "/checkpoint{}.pt".format(epoch // 10))
-            torch.save(network, PATH + "/model.pt")
+            "epoch": epoch,
+            "model_state_dict": network.state_dict,
+            "optim_state_dict": optimizer.state_dict,
+            "loss": loss
+            }, model_path + "/checkpoint{}.pt".format(epoch // 10))
+            torch.save(network, model_path + "/model.pt")
 
-    torch.save({
-        "epoch": epoch,
-        "model_state_dict": network.state_dict,
-        "optim_state_dict": optimizer.state_dict,
-        "loss": loss
-        }, PATH + "/checkpointfinal.pt")
-    torch.save(network, PATH + "/model.pt")
+torch.save({
+"epoch": epoch,
+"model_state_dict": network.state_dict,
+"optim_state_dict": optimizer.state_dict,
+"loss": loss
+}, model_path + "/checkpointfinal.pt")
+torch.save(network, model_path + "/model.pt")
 
+print("Finished!")
 
-    return network
-
-X = generate_input_grid()
-y = generate_labels(X)
-X_val = X[18000:]
-y_val = y[18000:]
-X = X[0:18000]
-y = y[0:18000]
