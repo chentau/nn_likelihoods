e6b4360351994b3afd83e20378ee4b02c14a74b6
Alexander Fengler <afengler@node473.oscar.ccv.brown.edu>
2019-07-31 15:19:51 -0400

    accidentally deleted a file

---
+++ ddm_nn_mcmc.py
@@ -0,0 +1,161 @@
+import numpy as np
+import pickle
+from cddm_data_simulation import ddm_simulate
+from samplers import *
+from tensorflow import keras
+
+print("loading neural network model")
+
+network_path = "/home/tony/repos/temp_models/keras_models/\
+dnnregressoranalytical_ddm_07_26_19_15_43_44/model_final.h5"
+
+model = keras.models.load_model(network_path)
+
+print("successfully loaded")
+
+def extract_info(model):
+    biases = []
+    activations = []
+    weights = []
+    for layer in model.layers:
+        if layer.name == "input_1":
+            continue
+        weights.append(layer.get_weights()[0])
+        biases.append(layer.get_weights()[1])
+        activations.append(layer.get_config()["activation"])
+    return weights, biases, activations
+
+weights, biases, activations = extract_info(model)
+
+def relu(x):
+    return np.maximum(0, x)
+
+def sigmoid(x):
+    return 1 / (1 + np.exp((-1) * x))
+
+def linear(x):
+    return x
+
+activation_fns = {
+        "relu": relu,
+        "sigmoid": sigmoid,
+        "linear": linear
+        }
+
+def np_predict(x, weights, biases, activations):
+    """
+    Redefine the keras .predict function in numpy
+
+    Params
+    ------
+    x: np.array((*batch_size, n_input))
+        inputs to calculate the prediction for.
+        batch size is optional
+    weights: list of np.ndarray((n_input, n_hidden))
+        list of the weight matrices for each layer
+    biases: list of np.ndarray((n_hidden, ))
+        list of the biases for each layer. Must be of
+        same length as the weights list
+    activations: list of strings
+        list of the activations to apply for each layer
+    """
+    num_layers = len(weights)
+    out = x
+    for l in range(num_layers):
+        out = np.dot(out, weights[l])
+        out += biases[l]
+        out = activation_fns[activations[l]](out)
+    return out
+
+def informative_wrapper(true_params):
+    v_dist = ss.norm(true_params[0], .1)
+    a_dist = ss.norm(true_params[1], .1)
+    z_dist = ss.norm(true_params[2], .1)
+    def out(fn):
+        def informative(params, data):
+            out = fn(params, data)
+            out += v_dist.logpdf(params[0])
+            out += a_dist.logpdf(params[1])
+            out += z_dist.logpdf(params[2])
+            return out
+        return informative
+    return out
+
+def informative_target(params, data):
+    out = target(params, data)
+    out += v_dist.logpdf(params[0])
+    out += a_dist.logpdf(params[1])
+    out += z_dist.logpdf(params[2])
+    return out
+
+def target(params, data):
+    params_rep = np.tile(params, (data.shape[0], 1))
+    input_batch = np.concatenate([params_rep, data], axis=1)
+    out = np_predict(input_batch, weights, biases, activations)
+    return out.sum()
+
+def target_array(params, data):
+    params_rep = np.tile(params, (data.shape[0], 1))
+    input_batch = np.concatenate([params_rep, data], axis=1)
+    out = np_predict(input_batch, weights, biases, activations)
+    return out
+
+def generate_data(n = 2000):
+    v = np.random.uniform(-1, 1)
+    a = np.random.uniform(.5, 1.5)
+    w = np.random.uniform(.3, .7)
+    rts, choices, _ = ddm_simulate(v, a, w, n_samples=2000)
+    data = np.concatenate([rts, choices], axis=1)
+    return ((v,a,w), data)
+
+def test_mh_sampling(num_iter=1000, proposal_var=.001):
+    true_params, data = generate_data()
+    model = MetropolisHastingsParallel(dims=3, num_chains=4,
+            bounds=[np.array([-1, 0, .3]), np.array([1, 1.5, .7])],
+            target=target, proposal_var=proposal_var)
+    model.sample(data, num_iter=num_iter)
+    return model, true_params
+
+def test_mh_adaptive(num_iter=1000, proposal_var=.001):
+    true_params, data = generate_data()
+    model = MetropolisHastingsAdaptive(dims=3, num_chains=4,
+            bounds=[np.array([-1, 0, .3]), np.array([1, 1.5, .7])],
+            target=target, proposal_var=proposal_var)
+    model.sample(data, num_iter=num_iter)
+    return model, true_params
+
+def test_mh_componentwise(num_iter=1000, proposal_var=.001):
+    true_params, data = generate_data()
+    model = MetropolisHastingsComponentwise(dims=3, num_chains=4,
+            bounds=[np.array([-1, .5, .3]), np.array([1, 1.5, .7])],
+            target=target, proposal_var=proposal_var)
+    model.sample(data, num_iter=num_iter)
+    return model, true_params
+
+def test_de_sampling(NP=7, num_iter=200, gamma=.1, proposal_var=.001):
+    true_params, data = generate_data()
+    model = DifferentialEvolutionParallel(dims=3,
+            bounds=[np.array([-1, 0, .3]), np.array([1, 1.5, .7])],
+            NP=NP, target=target, gamma=gamma, proposal_var=proposal_var)
+    model.sample(data, num_iter=num_iter)
+    return model, true_params
+
+def test_particle_filter(num_particles=5000, num_iter=10, proposal_var=.001):
+    true_params, data = generate_data()
+    model = ParticleFilterParallel(num_particles, [(-1, 1), (.5, 2), (.3, .7)], 
+                target, proposal_var)
+    model.sample(data, num_iter=num_iter)
+    return model, true_params
+
+def test_slice_sampling(num_samples = 2000, w = .4 / 256, p = 8):
+    true_params, data = generate_data()
+    model = SliceSampler(bounds=np.array([[-1, 1], [.5, 1.5], [.3, .7]]),
+            target=target, w = w, p = p)
+    model.sample(data, num_samples=num_samples)
+    return model, true_params
+
+def test_importance_sampling(num_particles=10000):
+    true_params, data = generate_data()
+    model = ImportanceSampling(bounds=np.array([[-1, 1], [.5, 1.5], [.3, .7]]), target = target)
+    model.sample(data, num_particles=num_particles)
+    return model, true_params
