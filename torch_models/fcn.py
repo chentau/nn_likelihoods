dd147b17ed5870e81624488db5db9b9a89ae9c81
Tony Chen <tchen1998@gmail.com>
2019-08-21 15:17:09 -0400

    more changes to fcn

--- torch_models/fcn.py
+++ torch_models/fcn.py
@@ -1,21 +1,22 @@
 import torch
 import torch.nn as nn
-import torch.nn.Functional as F
+import torch.nn.functional as F
 import numpy as np
 
 class FullyConvolutional(nn.Module):
-    def __init__(self, n_features, input_shape, filters=[64, 64, 128, 128, 128],
-            strides=[1, 2, 2, 2, 2]):
+    def __init__(self, input_shape, filters=[64, 64, 128, 128, 128],
+            strides=[1, 1, 1, 1, 1]):
         super(FullyConvolutional, self).__init__()
         self.n_conv_layers = len(filters)
         self.layers = nn.ModuleList([nn.Conv1d(input_shape, filters[0],
-            stride=stride[0])])
-        for i, filter, stride in enumerate(zip(filters[1:], strides[1:])):
-            self.layers.append(nn.Conv1d(filters[i-1], filter, stride=stride))
-        self.layers.append(nn.AvgPool1d(filters[-1]))
-        self.layers.append(nn.Linear(filters[-1], 1))
+            kernel_size=1, stride=strides[0])])
+        for i, layer in enumerate(zip(filters[1:], strides[1:]), 1):
+            self.layers.append(nn.Conv1d(filters[i-1], layer[0],
+                kernel_size=1, stride=layer[1]))
+        self.fc = nn.Linear(filters[-1], 1)
 
     def forward(self, x):
         for layer in self.layers:
             x = layer(x)
-        return x
+        x = x.sum(2)
+        return self.fc(x)
